d3dfa05089ca5a88d2918020aedc8f0cb525df21
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-09-04 17:30:53 -0300

    (feat/ homepage) add textbox & fixing gifs

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,25 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./HomePage.module.css";
+import PropTypes from "prop-types";
+
+const gifs = [
+  { src: "public/bucle-night-1.gif", type: "loop" },
+  { src: "public/bucle-night-2.gif", type: "transition" },
+  { src: "public/bucle-night-3.gif", type: "loop" },
+  // { src: 'url_del_gif_transicion_2.gif', type: 'transition' }
+];
+
+function GifPlayer({ gifs, currentGifIndex, setCurrentGifIndex }) {
+  useEffect(() => {
+    if (gifs[currentGifIndex].type === "transition") {
+      const img = new Image();
+      img.src = gifs[currentGifIndex].src;
+      img.onload = function () {
+        const gifDuration = 1200;
+        setTimeout(() => {
+          setCurrentGifIndex((prevIndex) => (prevIndex + 1) % gifs.length);
+        }, gifDuration);
+      };
+    }
+  }, [currentGifIndex, gifs]);
+
+  const handlerClick = () => {
+    if (gifs[currentGifIndex].type === "loop") {
+      setCurrentGifIndex((prevIndex) => (prevIndex + 1) % gifs.length);
+    }
+  };
+  return (
+    <img
+      className={styles.gifAnimated}
+      src={gifs[currentGifIndex].src}
+      alt="gif"
+      onClick={handlerClick}
+    />
+  );
+}
+
+GifPlayer.propTypes = {
+  gifs: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  currentGifIndex: PropTypes.number.isRequired,
+  setCurrentGifIndex: PropTypes.func.isRequired,
+};
 
 function HomePage() {
-  const [inputClick, setInputClick] = useState(0);
-
+  const [currentGifIndex, setCurrentGifIndex] = useState(0);
 
   return (
-    <>
-      <input
-        type="text"
-        value={inputClick}
-        onClick={() => setInputClick(inputClick < 3 ?  inputClick + 1 : 3)}
-        className={styles.invisibleInput}
+    <div className={styles.container}>
+      <GifPlayer
+        setCurrentGifIndex={setCurrentGifIndex}
+        currentGifIndex={currentGifIndex}
+        gifs={gifs}
       />
-     {inputClick == 0 && (
-        <div className={styles.initialAnimation}/>)}
-     {inputClick == 1 && (
-        <div className={styles.transitionAnimation}/>)}
-     {inputClick == 2 && (
-        <div className={styles.grettingsAnimation}/>)}
-    </>
+      {currentGifIndex == 2 && (
+        <img
+          className={styles.textBox}
+          src="public/textbox.svg"
+          alt="text-box"
+        />
+      )}
+    </div>
   );
 }
 

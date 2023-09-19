219b6be154b9c779f051ae0a2d21057f6c99fbeb
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-09-18 22:55:09 -0300

    feat(iconFolder) add setProject

--- src/components/IcoProjects/IcoProjects.jsx
+++ src/components/IcoProjects/IcoProjects.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
-function IconFolder({ title, imgWidth, imgPosition }) {
+function IconFolder({ title, imgWidth, imgPosition, onProject }) {
   return (
-    <div className="relative items-center w-fit h-fit m-5 hover:-translate-y-1 transition-transform">
+    <div
+      onClick={() => onProject(title)}
+      className="relative items-center w-fit h-fit m-5 hover:-translate-y-1 transition-transform">
       <img className="w-20" src="/carpeta-win.png" alt="background" />
       <img
         className={`absolute ${imgPosition || "top-7 left-6"} z-10 ${

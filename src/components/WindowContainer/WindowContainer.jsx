358548987fbaa99567d7cb98862f97d90c31ac42
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-09-18 22:57:04 -0300

    feat(windowContainer) add function

--- src/components/WindowContainer/WindowContainer.jsx
+++ src/components/WindowContainer/WindowContainer.jsx
@@ -1,45 +1,34 @@
+import { useState } from "react";
 import IconFolder from "../IcoProjects/IcoProjects";
+import WindowHeader from "../WindowHeader/WindowHeader";
+import ProjectContainer from "../ProjectContainer/ProjectContainer";
 
 /* eslint-disable react/prop-types */
 const WindowContainer = ({ onSelect, title }) => {
+  const [project, setProject] = useState(null);
   return (
     <div className="absolute top-10 flex-col h-[90vh] w-[90vw] sm:w-[80vw] bg-slate-50 z-10 shadow-neutral-700 shadow-md">
-      <div className="flex items-center justify-between bg-amber-200">
-        <div className="flex h-8 justify-center items-center">
-          <img className="h-4 m-2" src={`/${title}.png`} alt={title} />
-          <h1 className="text-sm text-black capitalize">{title}</h1>
-        </div>
-        <div className="flex">
-          <div
-            onClick={() => onSelect(null)}
-            className="relative bg-transparent border-none py-2 px-1 m-0 text-inherit font-inherit h-10 w-11 hover:bg-amber-300 hover:border-none focus:outline-none transition duration-150 ease-in-out">
-            <img
-              src="/minimize.png"
-              alt="minimize"
-              className="w-full h-full px-1"
+      <WindowHeader onSelect={onSelect} title={title} />
+      {title === "proyectos" && (
+        <section className="flex">
+          <div className="flex h-full w-full sm:w-[45%]">
+            <IconFolder
+              title="R&M"
+              imgWidth="w-9"
+              imgPosition="top-6 left-6"
+              onProject={setProject}
+            />
+            <IconFolder title="pokedex" onProject={setProject} />
+            <IconFolder
+              title="artify"
+              imgWidth="w-14"
+              imgPosition="top-7 left-3"
+              onProject={setProject}
             />
           </div>
-          <div
-            onClick={() => onSelect(null)}
-            className="relative bg-transparent border-none p-2 m-0 text-inherit font-inherit h-10 w-12 hover:bg-red-600 focus:outline-none transition duration-150 ease-in-out ">
-            <img src="/close.png" alt="close" className="w-full h-full px-1" />
-          </div>
-        </div>
-      </div>
-      {title === "proyectos" && (
-        <div className="flex">
-          <IconFolder
-            title="R&M"
-            imgWidth="w-9"
-            imgPosition="top-6 left-6"
-          />
-          <IconFolder title="pokedex"/>
-          <IconFolder
-            title="artify"
-            imgWidth="w-14"
-            imgPosition="top-7 left-3"
-          />
-        </div>
+
+          {project && <ProjectContainer title={project} onSelect={onSelect} />}
+        </section>
       )}
     </div>
   );

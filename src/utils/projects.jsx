11b5891c1d5da2356f4c5a81fa9f4ec4bec61c58
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-09-25 13:21:08 -0300

    add projects descriptions

---
+++ src/utils/projects.jsx
@@ -0,0 +1,100 @@
+const projects = {
+  artify: {
+    detail: (
+      <>
+        <h2 className="">
+          Cuenta con un home donde se muestran todas las publicaciones con
+          filtros y barra de búsqueda, login, recuperar contraseña, perfil de
+          usuarios, sistema de reseñas, carrito de compras, pasarela de pagos,
+          sistema de notificaciones por correo, panel de control de usuario y
+          administrador con métricas incluidas.
+        </h2>
+        <section>
+          <div className="flex items-center">
+            <h1 className="text-base font-bold pr-1">Front-End:</h1>
+            <h3 className="text-sm"> JavaScript, Next.js, Tailwind.</h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Back-End:</h1>
+            <h3 className="text-sm">Prisma, NodeJs.</h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Base de datos:</h1>
+            <h3 className="text-sm"> PostgreSQL. </h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base font-bold pr-1">Frameworks:</h1>
+            <h3 className="text-sm">
+              React, Next Auth, Redux Toolkit, Nodemailer, Uploadthing, Checkout
+              Pro (MP)
+            </h3>
+          </div>
+        </section>
+      </>
+    ),
+    overview: <h1 className="text-base sm:text-xl" >Plataforma Ecomerce</h1>,
+  },
+  pokedex: {
+    detail: (
+      <>
+        <h2 className="">
+          Donde cuenta con un home con paginado para explorar los distintos
+          Pokémons filtrándolos ordenándolos, buscando por barra de búsqueda Un
+          detalle de cada Pokémon con sus estadísticas Un apartado para crear un
+          nuevo Pokémon y cargar sus estadísticas
+        </h2>
+        <section>
+          <div className="flex items-center">
+            <h1 className="text-base font-bold pr-1">Front-End:</h1>
+            <h3 className="text-sm"> JavaScript, Vite.js, CSS.</h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Back-End:</h1>
+            <h3 className="text-sm">NodeJs, Express.Js, Sequelize.</h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Base de datos:</h1>
+            <h3 className="text-sm"> PostgreSQL. </h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Frameworks:</h1>
+            <h3 className="text-sm">React, Redux.</h3>
+          </div>
+        </section>
+      </>
+    ),
+    overview: <h1>App de enciclopedia de Pokémons</h1>,
+  },
+  rym: {
+    detail: (
+      <>
+        <h2 className="">
+          Donde cuenta con un home con paginado para explorar los distintos
+          personajes filtrándolos ordenándolos, buscando por barra de búsqueda Un
+          detalle de cada personaje con sus estadísticas. Un apartado para agregar como favorito un personaje.
+        </h2>
+        <section>
+          <div className="flex items-center">
+            <h1 className="text-base font-bold pr-1">Front-End:</h1>
+            <h3 className="text-sm"> JavaScript, Vite.js, CSS.</h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Back-End:</h1>
+            <h3 className="text-sm">NodeJs, Express.Js, Sequelize.</h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Base de datos:</h1>
+            <h3 className="text-sm"> PostgreSQL. </h3>
+          </div>
+          <div className="flex items-center">
+            <h1 className="text-base  font-bold pr-1">Frameworks:</h1>
+            <h3 className="text-sm">React, Redux.</h3>
+          </div>
+        </section>
+      </>
+    ),
+    overview: <h1>App de Rick & Morty</h1>,
+  },
+};
+
+export default projects

8f14a0e19aad58705eff2a9fecf504f1305908fd
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-10-04 17:07:44 -0300

    add new gifplayer(vignetteText)

--- src/components/VignetteText.jsx
+++ src/components/VignetteText.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 
-function VignetteText({ text }) {
+function VignetteText({ text, gifs }) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isTransition, setIsTransition] = useState(false);
   const firstIndex = currentIndex === 0;
   const lastIndex = currentIndex === text.length - 1;
 
@@ -18,36 +19,52 @@ function VignetteText({ text }) {
     setCurrentIndex(newIndex);
   };
 
-  useEffect(() => {}, [currentIndex]);
+  useEffect(() => {
+    setIsTransition(true);
+    const transition = setTimeout(() => {
+      setIsTransition(false);
+    }, 4200);
+    return () => {
+      clearTimeout(transition);
+    };
+  }, [currentIndex]);
 
   return (
-    <div className="flex flex-col justify-around items-center bg-slate-50 border-4 border-zinc-950 text-black font-minecraft  rounded-t-3xl rounded-br-3xl">
-      <div className="pt-5 sm:py-5 sm:mx-10 text-xl ">{text[currentIndex]}</div>
+    <>
+      <section className="text-xs xsm:text-base sm:text-base md:text-xl lg:text-3xl bg-slate-50 border-4 border-zinc-950 text-black font-minecraft rounded-t-3xl rounded-br-3xl shadow-md shadow-black ">
+        <div className="pt-5 mx-5 sm:mx-10 ">{text[currentIndex]}</div>
 
-      <div className="flex justify-end w-full">
-        <button
-          disabled={firstIndex}
-          className={`flex p-1 z-50 cursor-pointer hover:border-0 focus:outline-none hover:outline-none border-0 ${
-            firstIndex ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          onClick={goToPrevious}>
-          <img
-            className="transform scale-x-[-1] h-10"
-            src="/arrow.png"
-            alt="arrow"
-          />
-        </button>
+        <div className="flex justify-end w-full">
+          <button
+            disabled={firstIndex}
+            className={`flex p-1 z-50 cursor-pointer hover:border-0 focus:outline-none hover:outline-none border-0 ${
+              firstIndex ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            onClick={goToPrevious}>
+            <img
+              className="transform scale-x-[-1] h-10"
+              src="/arrow.png"
+              alt="arrow"
+            />
+          </button>
 
-        <button
-          disabled={lastIndex}
-          className={`flex p-1  z-50 cursor-pointer hover:border-0 focus:outline-none hover:outline-none border-0 ${
-            lastIndex ? "opacity-50 cursor-not-allowed" : ""
-          }`}
-          onClick={goToNext}>
-          <img className="h-10" src="/arrow.png" alt="arrow" />
-        </button>
-      </div>
-    </div>
+          <button
+            disabled={lastIndex}
+            className={`flex p-1  z-50 cursor-pointer hover:border-0 focus:outline-none hover:outline-none border-0 ${
+              lastIndex ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            onClick={goToNext}>
+            <img className="h-10" src="/arrow.png" alt="arrow" />
+          </button>
+        </div>
+      </section>
+      <section className="absolute bottom-0 left-20 z-50">
+        <img
+          src={isTransition ? gifs[1].src : gifs[0].src}
+          className="h-40 xsm:h-60 sm:h-[25rem]"
+        />
+      </section>
+    </>
   );
 }
 

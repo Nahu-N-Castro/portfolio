94fe3bcaf29c9ab8e155f02c60fad080642da529
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-10-04 11:42:47 -0300

    add vignett(about)

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import GifPlayer from "../GifPlayer/GifPlayer";
 import BackgroundParticles from "../BackgroundParticles";
 import style from "./About.module.css";
+import VignetteText from "../VignetteText";
 // import TextReveal from "../TextReveal/TextReveal";
 
 const gifsMe = [
@@ -12,16 +13,65 @@ const gifsMe = [
   { src: "/standby.gif", type: "loop" },
 ];
 
+const textAbout = [
+  [
+    <span key={1} className="animate-fade-right">
+      Hola Mundo!
+    </span>,
+  ],
+  [
+    <span key={2} className="animate-fade-right">
+      Soy Nahuel, un apasionado Desarrollador Web formado en el Bootcamp de
+      SoyHenry y estudiante de Ingeniería en Sistemas de Información en la UTN
+      FRC. Mi primer acercamiento a la programación fue como Técnico
+      Electrónico, trabajando con Arduino. Esta experiencia despertó en mí un
+      profundo interés por la tecnología y su potencial para resolver problemas
+      cotidianos de maneras innovadoras y eficientes.
+    </span>,
+  ],
+  [
+    <span key={3} className="animate-fade-right">
+      Adopto una mentalidad crítica y curiosa, disfrutando del desafío de
+      mejorar código y descubrir soluciones innovadoras . Mi formación en
+      ingeniería y la intensidad del Bootcamp de SoyHenry han potenciado mi
+      habilidad para enfrentar desafíos y resolver problemas de manera
+      eficiente. Además, mis experiencias académicas y prácticas han cultivado
+      mi capacidad para colaborar y liderar equipos , fomentando un ambiente
+      de trabajo productivo y cooperativo.
+    </span>,
+  ],
+  [
+    <span key={4} className="animate-fade-right">
+      Veo la comunicación  como un pilar fundamental que me permite trabajar
+      eficazmente tanto en equipos como en proyectos individuales. Además,
+      valoro mantener una organización  clara y eficiente.
+    </span>,
+  ],
+  [
+    <span key={5} className="animate-fade-right">
+      Me emociona la oportunidad de aplicar mi pasión  por la programación, mi
+      curiosidad innata y mi constante deseo de aprender en nuevos proyectos y
+      desafíos. Cada paso en este camino lo veo como una oportunidad para crecer
+       y continuar desarrollándome profesionalmente. Estoy interesado en
+      unirme a un equipo que valore la creatividad, la innovación y la
+      colaboración, y esté comprometido con ofrecer resultados de alta calidad a
+      sus clientes.
+    </span>,
+  ],
+  [
+    <span key={5} className="animate-fade-right">
+      Si estás buscando a alguien con habilidades en JavaScript, React, Redux,
+      Next.js, CSS, Tailwind, Figma, Express, PostgreSQL y Git, no dudes en
+      contactarme . ¡Será un placer trabajar juntos!
+    </span>,
+  ],
+];
+
 function About() {
   const [currentGifIndex, setCurrentGifIndex] = useState(0);
-  // const text = [
-  //   { string: "Hola, ", delay: 1000, classes: "px-1" },
-  //   { string: "soy ", delay: 500, classes: "px-1" },
-  //   { string: "Nahuel ", delay: 800, classes: "text-red-500 px-1" },
-  //   { string: "un desarollador web! ", delay: 700, classes: "px-1" },
-  // ];
+
   return (
-    <section className="grid grid-rows-2 h-full w-full z-10">
+    <section className="flex flex-col h-full w-full z-10 sm:p-10">
       <div className={style.container}>
         <span style={{ "--l": "'N'" }}>N</span>
         <span style={{ "--l": "'A'" }}>A</span>
@@ -31,13 +81,15 @@ function About() {
         <span style={{ "--l": "'L'" }}>L</span>
       </div>
 
-      <div className="inline-block absolute top-[15%] sm:top-[27%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-minecraft text-[#F5F1DE] text-lg sm:text-4xl">
+      <div className="flex justify-center pb-5 sm:pb-10 font-minecraft text-[#F5F1DE] text-lg sm:text-4xl">
         <span className={style.dev} style={{ "--d": "'Full Stack Developer'" }}>
           Full Stack Developer
         </span>
       </div>
 
-      {/* <TextReveal text={text} /> */}
+      <div className="flex h-[30rem] sm:h-[20rem] sm:mx-64  items-end justify-start">
+        {<VignetteText text={textAbout} />}
+      </div>
 
       <div className="absolute -bottom-3 mx-10 h-60 sm:h-96 z-50">
         <GifPlayer
@@ -47,7 +99,6 @@ function About() {
           gifs={gifsMe}
         />
       </div>
-
       <BackgroundParticles widthRem={50} heightRem={40} />
     </section>
   );

d44db3465f5876197a5ccc1551764e3c1d83b5b5
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-10-03 22:46:08 -0300

    fix about, add gifplayer w/ gifduration

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,28 +7,31 @@ function About() {
   const [currentGifIndex, setCurrentGifIndex] = useState(0);
   const gifsMe = [
     { src: "/standby.gif", type: "loop" },
-    // { src: "/talking.gif", type: "transition" },
+    { src: "/talking.gif", type: "transition" },
+    { src: "/standby.gif", type: "loop" },
+    { src: "/talking.gif", type: "transition" },
+    { src: "/standby.gif", type: "loop" },
   ];
   return (
-    <section className="grid grid-row-2 h-full w-full z-10">
+    <section className="grid grid-rows-2 h-full w-full z-10">
       <div className={style.container}>
-        {/* <span style={{ "--l": "'N'" }}>N</span>
+        <span style={{ "--l": "'N'" }}>N</span>
         <span style={{ "--l": "'A'" }}>A</span>
         <span style={{ "--l": "'H'" }}>H</span>
         <span style={{ "--l": "'U'" }}>U</span>
         <span style={{ "--l": "'E'" }}>E</span>
-        
-        <span style={{ "--l": "'L'" }}>L</span> */}
-        <span style={{ "--l": "'W'" }}>W</span>
-        <span style={{ "--l": "'A'" }}>A</span>
         <span style={{ "--l": "'L'" }}>L</span>
-        <span style={{ "--l": "'K'" }}>K</span>
-        <span style={{ "--l": "'E'" }}>E</span>
-        <span style={{ "--l": "'R'" }}>R</span>
       </div>
 
-      <div className="absolute -bottom-3 mx-10 h-96">
+      <div className="absolute top-[27%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-minecraft text-[#F5F1DE] text-4xl">
+        <span className={style.dev} style={{ "--d": "'Full Stack Developer'" }}>
+          Full Stack Developer
+        </span>
+      </div>
+
+      <div className="absolute -bottom-3 mx-10 h-96 z-50">
         <GifPlayer
+          gifDuration={4200}
           setCurrentGifIndex={setCurrentGifIndex}
           currentGifIndex={currentGifIndex}
           gifs={gifsMe}

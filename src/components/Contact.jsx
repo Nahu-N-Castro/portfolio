f3f52823ff1332e9db2a05fa6b65ed895a2ed51f
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-10-04 18:39:50 -0300

    fix icos(contact)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,12 @@
 const Contact = () => {
   return (
-    <div className="flex justify-center items-center w-full h-full space-x-64 z-50">
+    <div className="sm:flex sm:justify-center md:items-center w-full h-full pt-10 grid grid-cols-1 sm:space-x-10 md:space-x-64 z-50">
+      <a
+        href="mailto:nahu.castro@hotmail.com"
+        className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
+        <img src="/contacto.png" className="h-20 w-20 sm:h-52 sm:w-52 " />
+        <span>Mail</span>
+      </a>
       <a
         href="https://github.com/Nahu-N-Castro"
         target="_blank"
@@ -8,15 +14,9 @@ const Contact = () => {
         className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-          className="h-52"
+          className="h-20 sm:h-52"
         />
         <span>GitHub</span>
-      </a>
-      <a
-        href="mailto:nahu.castro@hotmail.com"
-        className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
-        <img src="/contacto.png" className="h-52" />
-        <span>Mail</span>
       </a>
 
       <a
@@ -26,7 +26,7 @@ const Contact = () => {
         className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
-          className="h-52"
+          className="h-20 sm:h-52"
         />
         <span>Linkedin</span>
       </a>

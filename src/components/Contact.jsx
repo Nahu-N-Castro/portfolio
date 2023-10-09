9f58f2dda17313599de3016c33e150797de5c5fd
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-10-09 16:47:17 -0300

    re-style (contact)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     <div className="sm:flex sm:justify-center md:items-center w-full h-full pt-10 grid grid-cols-1 sm:space-x-10 md:space-x-64 z-50">
       <a
         href="mailto:nahu.castro@hotmail.com"
-        className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
+        className="flex flex-col text-white hover:text-red-400 items-center gap-5 transition-transform transform hover:scale-110">
         <img src="/contacto.png" className="h-20 w-20 sm:h-52 sm:w-52 " />
         <span>Mail</span>
       </a>
@@ -11,7 +11,7 @@ const Contact = () => {
         href="https://github.com/Nahu-N-Castro"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
+        className="flex flex-col text-white hover:text-red-400 items-center gap-5 transition-transform transform hover:scale-110">
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
           className="h-20 sm:h-52"
@@ -23,7 +23,7 @@ const Contact = () => {
         href="https://www.linkedin.com/in/nahuelcastro/"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex flex-col text-white hover:text-gray-500 items-center gap-5">
+        className="flex flex-col text-white hover:text-red-400 items-center gap-5 transition-transform transform hover:scale-110">
         <img
           src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg"
           className="h-20 sm:h-52"

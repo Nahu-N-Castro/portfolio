7df0d39f0df444418f09c85a2b6ac0cae8e65e8a
Nahu-N-Castro <nahu.castro208@gmail.com>
2023-10-10 13:14:45 -0300

    re-style projects

--- src/utils/projects.jsx
+++ src/utils/projects.jsx
@@ -11,47 +11,59 @@ const projects = {
             filtros avanzados.
           </p>
           <p className="my-1">
-            Los usuarios pueden registrarse en Artify a través de Google o
-            creando una cuenta localmente. En caso de olvidar la contraseña,
-            cuentan con una opción para recuperarla.
+            Los usuarios pueden registrarse en Artify a través de{" "}
+            <strong className="text-rose-400">
+              Google o creando una cuenta localmente.
+            </strong>{" "}
+            En caso de olvidar la contraseña, cuentan con una opción para
+            recuperarla.
           </p>
           <p className="my-1">
             La plataforma presenta una pantalla principal donde se exhiben las
-            publicaciones actuales, y se incluye una barra de búsqueda para
+            publicaciones actuales, y se incluye una{" "}
+            <strong className="text-rose-400">barra de búsqueda</strong> para
             facilitar la localización de productos.
           </p>
           <p className="my-1">
-            Artify otorga a los artesanos la posibilidad de tener su propio
-            perfil, en el cual se muestra su número de teléfono, redes sociales
-            y sus respectivas publicaciones. Además, las publicaciones en Artify
-            disponen de un sistema de reseñas, permitiendo a los usuarios
-            informarse previamente y, posterior a una compra, proporcionar su
-            propia reseña.
+            Artify otorga a los artesanos la posibilidad de tener su propio {""}
+            <strong className="text-rose-400">perfil</strong>, en el cual se
+            muestra su número de teléfono, redes sociales y sus respectivas
+            publicaciones. Además, las publicaciones en Artify disponen de un{" "}
+            <strong className="text-rose-400">sistema de reseñas</strong>,
+            permitiendo a los usuarios informarse previamente y, posterior a una
+            compra, proporcionar su propia reseña.
           </p>
           <p className="my-1">
             Los visitantes pueden navegar por la página libremente, añadir uno o
-            varios productos al carrito y, previo a realizar la transacción,
-            editar la lista de productos seleccionados. Para asegurar
-            transacciones confiables y seguras, trabajamos en conjunto con
-            Mercado Pago.
+            varios productos al{" "}
+            <strong className="text-rose-400">carrito</strong> y, previo a
+            realizar la transacción, editar la lista de productos seleccionados.
+            Para asegurar transacciones confiables y seguras, trabajamos en
+            conjunto con {""}
+            <strong className="text-rose-400">Mercado Pago</strong>.
           </p>
           <p className="my-1">
-            Artify integra un sistema de notificaciones por correo, garantizando
-            que el usuario esté informado sobre sus acciones en la plataforma.
-            Tanto usuarios como vendedores cuentan con un panel de control donde
-            pueden visualizar distintos tipos de métricas para analizar sus
-            compras o ventas en el sitio.
+            Artify integra un{" "}
+            <strong className="text-rose-400">
+              sistema de notificaciones por correo
+            </strong>
+            , garantizando que el usuario esté informado sobre sus acciones en
+            la plataforma. Tanto usuarios como vendedores cuentan con un{" "}
+            <strong className="text-rose-400">panel de control</strong> donde
+            pueden visualizar distintos tipos de{" "}
+            <strong className="text-rose-400">métricas</strong> para analizar
+            sus compras o ventas en el sitio.
           </p>
 
           <section className="flex flex-col">
             <div className="flex flex-row">
-              <h2 className="font-bold mr-2">Stack: </h2>
+              <h2 className="font-bold mr-2 text-rose-400">Stack: </h2>
               <p className="italic">
                 JavaScript, Next.js, Tailwind, Prisma, NodeJs, PostgreSQL
               </p>
             </div>
             <div className="flex flex-row">
-              <h2 className="font-bold  mr-2">Frameworks: </h2>
+              <h2 className="font-bold mr-2 text-rose-400">Frameworks: </h2>
               <p className="italic">
                 React, Next Auth, Redux Toolkit, Nodemailer, Uploadthing,
                 Checkout Pro (MP)
@@ -84,26 +96,36 @@ const projects = {
 
           <ul className="list-disc pl-5 mb-3">
             <li>
-              Creación y prototipado de interfaces en <strong>Figma</strong>,
-              definiendo la experiencia de usuario y transformando ideas en
-              diseños visuales funcionales.
+              Creación y prototipado de interfaces en{" "}
+              <strong className="text-rose-400">Figma</strong>, definiendo la
+              experiencia de usuario y transformando ideas en diseños visuales
+              funcionales.
             </li>
             <li>
-              Implementación de diseños con <strong>Tailwind</strong>, lo que me
+              Implementación de diseños con{" "}
+              <strong className="text-rose-400">Tailwind</strong>, lo que me
               permitió construir un sitio web estético y altamente optimizado.
               El dominio de esta herramienta mejoró enormemente mis habilidades
               en diseño web y desarrollo frontend.
             </li>
             <li>
-              Integración de tecnologías como <strong>Next.js</strong> y{" "}
-              <strong>Next Auth</strong> para potenciar la funcionalidad y
-              seguridad del sitio.
+              Integración de tecnologías como{" "}
+              <strong className="text-rose-400">Next.js</strong> y{" "}
+              <strong className="text-rose-400">Next Auth</strong> para
+              potenciar la funcionalidad y seguridad del sitio.
             </li>
             <li>
-              Trabajo en equipo, donde la comunicación y colaboración fueron
-              esenciales para lograr los objetivos del proyecto. Esta
-              experiencia reforzó mi capacidad para colaborar en proyectos
-              multidisciplinarios y adaptarme a los desafíos en tiempo real.
+              Implementación del{" "}
+              <strong className="text-rose-400">Panel de Control </strong> y{" "}
+              <strong className="text-rose-400">Métricas</strong> para poder
+              darle control y información util al usuario.
+            </li>
+            <li>
+              <strong className="text-rose-400">Trabajo en equipo</strong>,
+              donde la comunicación y colaboración fueron esenciales para lograr
+              los objetivos del proyecto. Esta experiencia reforzó mi capacidad
+              para colaborar en proyectos multidisciplinarios y adaptarme a los
+              desafíos en tiempo real.
             </li>
           </ul>
 
@@ -145,14 +167,14 @@ const projects = {
 
           <section className="flex flex-col">
             <div className="flex flex-row">
-              <h2 className="font-bold mr-2">Stack: </h2>
+              <h2 className="font-bold mr-2 text-rose-400">Stack: </h2>
               <p className="italic">
                 JavaScript, Vite.js, CSS, NodeJs, Express.Js, Sequelize,
                 PostgreSQL
               </p>
             </div>
             <div className="flex flex-row">
-              <h2 className="font-bold  mr-2">Frameworks: </h2>
+              <h2 className="font-bold mr-2 text-rose-400">Frameworks: </h2>
               <p className="italic">React, Redux</p>
             </div>
           </section>
@@ -175,26 +197,30 @@ const projects = {
 
         <ul className="list-disc pl-5 mb-3">
           <li>
-            Adquirí destrezas avanzadas en <strong>JavaScript</strong>,
-            manejando programación asincrónica y manipulación del DOM.
+            Adquirí destrezas avanzadas en{" "}
+            <strong className="text-rose-400">JavaScript</strong>, manejando
+            programación asincrónica y manipulación del DOM.
           </li>
           <li>
-            Exploré <strong>Vite.js</strong> y sus optimizaciones para
-            desarrollo y producción.
+            Exploré <strong className="text-rose-400">Vite.js</strong> y sus
+            optimizaciones para desarrollo y producción.
           </li>
           <li>
-            Creé interfaces con <strong>CSS</strong> animaciones.
+            Creé interfaces con <strong className="text-rose-400">CSS</strong>{" "}
+            animaciones.
           </li>
           <li>
-            Configuré servidores con <strong>NodeJs & Express.Js</strong>,
+            Configuré servidores con{" "}
+            <strong className="text-rose-400">NodeJs & Express.Js</strong>,
             integrándome con bases de datos y otros servicios.
           </li>
           <li>
-            Manejé datos y relaciones en <strong>Sequelize & PostgreSQL</strong>
-            .
+            Manejé datos y relaciones en{" "}
+            <strong className="text-rose-400">Sequelize & PostgreSQL</strong>.
           </li>
           <li>
-            Construí componentes reactivos con <strong>React & Redux</strong>,
+            Construí componentes reactivos con{" "}
+            <strong className="text-rose-400">React & Redux</strong>,
             gestionando estado global y optimizando la experiencia del usuario.
           </li>
         </ul>
@@ -213,11 +239,12 @@ const projects = {
       <>
         <div className="text-white bg-neutral-800 border-neutral-800 border-[12px] rounded-b-xl rounded-tr-xl max-h-[25rem] sm:max-h-[10rem] overflow-y-scroll ">
           <p className="my-1">
-            Rick and Morty Integrator representó mi primera inmersión profunda
-            en el mundo del desarrollo web. A través de esta plataforma, los
-            usuarios pueden descubrir personajes de la serie, conocer detalles
-            sobre ellos y personalizar su experiencia con opciones de filtrado,
-            ordenamiento y búsqueda.
+            Rick and Morty Integrator representó{" "}
+            <strong className="text-rose-400">mi primera inmersión </strong>
+            profunda en el mundo del desarrollo web. A través de esta
+            plataforma, los usuarios pueden descubrir personajes de la serie,
+            conocer detalles sobre ellos y personalizar su experiencia con
+            opciones de filtrado, ordenamiento y búsqueda.
           </p>
           <p className="my-1">
             A pesar de ser mi primer proyecto, me esforcé por brindar una
@@ -227,14 +254,14 @@ const projects = {
           </p>
           <section className="flex flex-col mt-2">
             <div className="flex flex-row">
-              <h2 className="font-bold mr-2">Stack: </h2>
+              <h2 className="font-bold mr-2 text-rose-400">Stack: </h2>
               <p className="italic">
                 JavaScript, Vite.js, CSS, NodeJs, Express.Js, Sequelize,
                 PostgreSQL.
               </p>
             </div>
             <div className="flex flex-row">
-              <h2 className="font-bold mr-2">Frameworks: </h2>
+              <h2 className="font-bold mr-2 text-rose-400">Frameworks: </h2>
               <p className="italic">React, Redux.</p>
             </div>
           </section>
@@ -259,14 +286,16 @@ const projects = {
 
           <ul className="list-disc pl-5 mb-3">
             <li>
-              Descubrí el poder de <strong>React</strong> y{" "}
-              <strong>Redux</strong> y cómo estas herramientas pueden
-              transformar ideas en aplicaciones interactivas y dinámicas.
+              Descubrí el poder de{" "}
+              <strong className="text-rose-400">React y Redux</strong> y cómo
+              estas herramientas pueden transformar ideas en aplicaciones
+              interactivas y dinámicas.
             </li>
             <li>
-              Aprendí a consumir y trabajar con APIs, en este caso la de Rick
-              and Morty, lo que me dio una comprensión fundamental sobre la
-              gestión de datos y su presentación.
+              Aprendí a consumir y trabajar con{" "}
+              <strong className="text-rose-400">APIs</strong>, en este caso la
+              de Rick and Morty, lo que me dio una comprensión fundamental sobre
+              la gestión de datos y su presentación.
             </li>
             <li>
               Fortalecí mis fundamentos en diseño UX/UI, entendiendo la
